90740d32703c42c81b9b358644067b4e1e547383
leiwang80 <leiwang1688@yahoo.com>
2015-06-21 13:07:50 -0700

    Final results

--- server.R
+++ server.R
@@ -8,11 +8,9 @@ shinyServer(
   function(input, output) {
     y <- reactive(predict(modFit, newdata=data.frame(fheight=as.numeric(input$fheight)),
                           interval="confidence"))
- #   y <- reactive(as.numeric(input$fheight) +1)
-    output$sheight <- renderText({class(y)})
-    output$text2 <- renderText({y()[1]})
-    output$text3 <- renderText({y()[2]})
-    output$text4 <- renderText({y()[3]})
-    output$text5 <- renderText({y()[3]})
+
+    output$sheight <- renderText({y()[1]})
+    output$lwr <- renderText({y()[2]})
+    output$upr <- renderText({y()[3]})
   }
 )
--- ui.R
+++ ui.R
@@ -2,22 +2,36 @@ library(shiny)
 
 
 shinyUI(pageWithSidebar(
-  headerPanel("Hello Shiny!"),
+  headerPanel("Height Prediction for Sons"),
   sidebarPanel(
-    textInput(inputId="fheight", label = "The height of father in inch:"),
-    textInput(inputId="text2", label = "Input Text2"),
-    submitButton("Submit")
+    h4("Enter the height of father (unit inch):"),
+    textInput(inputId="fheight",label = ""),
+    submitButton("Submit"),
+    br(),
+    br(),
+    
+    h4("Predicted Results  (unit inch):"),
+    p('The predicted height of son:'),
+    textOutput('sheight'),
+    p('Lower bound of 95% confidence interval:'),
+    textOutput('lwr'),
+    p('Upper bound of 95% confidence interval:'),
+    textOutput('upr')
   ),
   mainPanel(
-    p('The predicted height of son in inch'),
-    textOutput('sheight'),
-    p('Output text2'),
-    textOutput('text2'),
-    p('Output text3'),
-    textOutput('text3'),
-    p('Outside text'),
-    textOutput('text4'),
-    p('Inside text, but non-reactive'),
-    textOutput('text5')
+    h3('Prediction Tool Information'),
+    
+    p("This is prediction tool to son\'s height based on father\'s height. 
+Just enter the father\'s in the input field on th upper left side, hit the submit button. 
+The predicted son\'s height, as well as the lower and upper bound of 
+95% conficdence interval will be displayed on the lower left site.
+    "),
+    
+    p("This prediction is using linear regression model.  
+      The data used to build this prediction model 
+      is the dataset father.son from UsingR package."),
+    
+    p("Make sure that you enter a numeric value for father\'s height, 
+      otherwise NA will displayed in the out fields.")
   )
 ))
